cd9ad5a89df4f32d02c9152a4b2803408fdeb15c
Ehobee <ritsu@abv.bg>
2022-06-04 12:55:15 +0300

    Synch local

--- gameEngine.js
+++ gameEngine.js
@@ -2,10 +2,39 @@ function start(state, game) {
 
     game.createSplatoon(state.splatoon);
 
-    window.requestAnimationFrame(gameLoop.bind(null, state, game));
+    window.requestAnimationFrame(gameLoop.bind(state, game));
 };
 
 function gameLoop() {
-    console.log('start')
-    window.requestAnimationFrame(gameLoop.bind(null, state, game));
+    const { splatoon } = state;
+    const { splatoonElement } = game;
+
+    mainCharacterMovement(state, game);
+    //Rendersplatoon
+    splatoonElement.style.left = splatoon.positionX + 'px';
+    splatoonElement.style.top = splatoon.positionY + 'px';
+
+    //Spawn squids
+    game.createSquid(state.squidStats);
+
+    window.requestAnimationFrame(gameLoop.bind(state, game));
 };
+
+function mainCharacterMovement(state, game) {
+    const { splatoon } = state;
+
+    //Main character movement
+    if (state.keys.KeyD && splatoon.positionX < (game.gameScreen.offsetWidth - splatoon.width - splatoon.speed)) {
+        splatoon.positionX += splatoon.speed;
+    };
+    if (state.keys.KeyA && splatoon.positionX > 1) {
+        splatoon.positionX -= splatoon.speed;
+    };
+    if (state.keys.KeyW && splatoon.positionY > 1) {
+        splatoon.positionY -= splatoon.speed;
+    };
+    if (state.keys.KeyS && splatoon.positionY < (game.gameScreen.offsetHeight - splatoon.height - splatoon.speed)) {
+        splatoon.positionY += splatoon.speed;
+    };
+
+}
--- gameState.js
+++ gameState.js
@@ -1,12 +1,21 @@
 function initState() {
+    let startX = Math.floor(Math.random() * 1000);
+    let startY = Math.floor(Math.random() * 500);
+
     const state = {
         player: "Ehobee",
         splatoon: {
             width: 130,
             height: 153,
-            startX: 200,
-            startY:200
-        }
+            positionX: startX,
+            positionY: startY,
+            speed: 10
+        },
+        squidStats: {
+            width: 100,
+            height: 100,
+        },
+        keys: {}
     };
 
     return state;
